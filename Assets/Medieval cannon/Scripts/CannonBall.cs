dc550e5bf852988fce8c601d827db79f8a3395c6
Smokeylope <vahatom@gmail.com>
2018-10-01 22:38:22 +0300

    Initial commit

---
+++ Assets/Medieval cannon/Scripts/CannonBall.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBall : MonoBehaviour {
+
+    public GameObject explosion;
+    public GameObject scoreText;
+    public float bounciness = 0.5f;
+    public float gravity = 4.0f;
+    public float explosionStrength = 20.0f;
+
+    [HideInInspector]
+    public Vector3 velocity;
+
+    private float destroyTimer = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+        //Destroy(this.gameObject, 5f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 g = new Vector3(0.0f, -gravity, 0.0f) * Time.deltaTime;
+        velocity += g;
+        transform.position += velocity * Time.deltaTime;
+
+        string score = ((int) Mathf.Sqrt(Mathf.Pow(transform.position.x, 2.0f) + Mathf.Pow(transform.position.z, 2.0f))).ToString();
+        scoreText.GetComponent<TextMesh>().text = score;
+
+        if (velocity.sqrMagnitude < 0.1f)
+        {
+            destroyTimer -= Time.deltaTime;
+        }
+        else
+        {
+            transform.rotation = Quaternion.LookRotation(velocity.normalized, Vector3.up);
+        }
+
+        if (destroyTimer < 0.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (velocity.y <= 0f)
+        {
+            if (other.gameObject.tag == "Spikes")
+            {
+                velocity = Vector3.zero;
+                gravity = 0.0f;
+            }
+            else if (other.gameObject.tag == "Explosive")
+            {
+                velocity.y = -velocity.y + explosionStrength;
+            }
+            else
+            {
+                transform.position = new Vector3(transform.position.x, 0.0f, transform.position.z);
+                velocity.y = -velocity.y;
+                velocity *= bounciness;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    public GameObject cannon;
+    public GameObject cannonBall;
+    public Vector3 positionOffset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+        GameObject target = cannonBall;
+        Vector3 offset = positionOffset;
+
+		if (target == null)
+        {
+            target = cannon;
+            offset = Vector3.zero;
+            transform.rotation = target.transform.rotation;
+        }
+        else
+        {
+            Vector3 direction = target.transform.position;
+            direction.y = 0.0f;
+            direction.Normalize();
+
+            transform.rotation = Quaternion.LookRotation(direction, Vector3.up); ;
+        }
+
+        
+        transform.position = target.transform.position + transform.rotation * offset;
+    }
+}
