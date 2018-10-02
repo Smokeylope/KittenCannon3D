d3089225f265417b6997ebf7c4b49d4cf00a066f
Smokeylope <vahatom@gmail.com>
2018-10-02 17:34:26 +0300

    Add level generation and clean up

---
+++ Assets/Scripts/LevelGenerator.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelGenerator : MonoBehaviour {
+
+    public int objectCount = 500;
+    public float minDistance = 5.0f;
+    public float maxDistance = 200.0f;
+
+    public List<GameObject> objects;
+
+	// Use this for initialization
+	void Start () {
+		for (int i = 0; i < objectCount; ++i)
+        {
+            float angle = Random.Range(0.0f, Mathf.PI * 2.0f);
+            float distance = Random.Range(minDistance, maxDistance);
+
+            Vector3 direction = new Vector3(Mathf.Cos(angle), 0.0f, Mathf.Sin(angle));
+            Vector3 position = direction * distance;
+
+            GameObject.Instantiate(objects[Random.Range(0, objects.Count)], position, Quaternion.LookRotation(direction, Vector3.up));
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
